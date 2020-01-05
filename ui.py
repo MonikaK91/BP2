f62332d88e0237532cf968f3bcf783d7457f935f
MonikaK91 <59527843+MonikaK91@users.noreply.github.com>
2020-01-05 11:35:31 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -357,7 +357,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    userI = Ui_MainWindow()
+    userI.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
